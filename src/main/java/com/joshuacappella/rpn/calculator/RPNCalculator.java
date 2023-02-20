0226f16fd7ae3726f896ae612fc8e5b13016b9f7
Joshua Cappella <Joshua Cappella@10.13.103.165>
2023-02-20 14:23:23 -0500

    Github test

--- src/main/java/com/joshuacappella/rpn/calculator/RPNCalculator.java
+++ src/main/java/com/joshuacappella/rpn/calculator/RPNCalculator.java
@@ -11,6 +11,6 @@ package com.joshuacappella.rpn.calculator;
 public class RPNCalculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello GitHub!");
     }
 }
