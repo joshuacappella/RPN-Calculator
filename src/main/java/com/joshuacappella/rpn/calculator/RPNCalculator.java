16f4a16edeefd072ec93f18e1027158c56acc0b0
Joshua Cappella <Joshua Cappella@10.13.103.165>
2023-02-20 11:34:15 -0500

    First commit!

---
+++ src/main/java/com/joshuacappella/rpn/calculator/RPNCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.joshuacappella.rpn.calculator;
+
+/**
+ *
+ * @author Joshua Cappella
+ */
+public class RPNCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
