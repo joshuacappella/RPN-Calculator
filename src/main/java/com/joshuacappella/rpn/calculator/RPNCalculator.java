6a230d2669f8cc2c198190d2840ab396e3a82210
Joshua Cappella <Joshua Cappella@10.13.103.165>
2023-02-24 13:48:39 -0500

    First draft complete

--- src/main/java/com/joshuacappella/rpn/calculator/RPNCalculator.java
+++ src/main/java/com/joshuacappella/rpn/calculator/RPNCalculator.java
@@ -4,13 +4,16 @@
 
 package com.joshuacappella.rpn.calculator;
 
+import static com.joshuacappella.rpn.calculator.compute.compute;
+
 /**
  *
  * @author Joshua Cappella
  */
 public class RPNCalculator {
-
+    public String yamaha = "23.3 5 16.2 + 8 * -";
     public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
+        System.out.println(compute(yamaha));
+        
     }
 }
--- src/main/java/com/joshuacappella/rpn/calculator/compute.java
+++ src/main/java/com/joshuacappella/rpn/calculator/compute.java
@@ -15,40 +15,47 @@ public class compute {
 
     /**
      *
-     * @param rpn
+     * @param args
      * @return
      */
     public static double compute(String[] args){
         double result = 0;
         String math = "";
-        Stack<Double> stack = new Stack<Double>();
+        Stack<Double> stack;
+        stack = new Stack<Double>();
         for(int i = 0; i < stack.size(); i++){
             Scanner itemN = new Scanner(System.in);
             String item = itemN.toString();
-            if((item != "+") && (item != "-") &&
-                    (item != "*") && (item != "/")){
+            if((!"+".equals(item)) && (!"-".equals(item)) &&
+                    (!"*".equals(item)) && (!"/".equals(item))){
                 double d = Double.parseDouble(item);
                 stack.push(d); 
             }
-            else if(("+" == item) || (item == "-") ||
-                    (item == "*") || (item == "/")){
+            else if(("+".equals(item)) || ("-".equals(item)) ||
+                    ("*".equals(item)) || ("/".equals(item))){
                 math += item;
             }
         }
+        double lastResult = stack.pop();
         while(math.length() != 0){
             if(math.charAt(0) == '+'){
-
+                result += lastResult;
             }
             else if(math.charAt(0) == '-'){
-
+                result -= lastResult;
             }
             else if(math.charAt(0) == '*'){
-
+                result *= lastResult;
             }
             else if(math.charAt(0) == '/'){
-
+                result /= lastResult;
             }
-            math = math.substring(1);
+            if(math.length()== 2){
+                math = math.substring(1);
+            }
+            else if(math.length() == 1){
+                math = "";
+            }
         }
         return result;
     }
